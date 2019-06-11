5086a8f43948aafcc047108c490ab8c7cb5978b5
mrdonghaibin <donghaibin@yeah.net>
2019-06-11 23:28:16 +0800

    V9

--- src/main/java/com/pingan/haibin/action/TestAction.java
+++ src/main/java/com/pingan/haibin/action/TestAction.java
@@ -13,7 +13,7 @@ public class TestAction {
 	  
     @RequestMapping("/hello")
     public String sayHello() {
-        return "hello zhangsan V0.2 Vhaibin2";
+        return "hello zhangsan V0.2 Vhaibin2 999";
     	
     }
     
