f95c9a10971663135c45ca8a1eb5ca2a19740a2e
mrdonghaibin <donghaibin@yeah.net>
2019-06-11 23:19:49 +0800

    master V0.7

--- src/main/java/com/pingan/haibin/action/Test.java
+++ src/main/java/com/pingan/haibin/action/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("11661");
+	
+		System.out.println("1166MasterV");
 	}
 
 }
