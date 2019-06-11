d50b37918f279e0729a9ad0abb75879077867ea2
mrdonghaibin <donghaibin@yeah.net>
2019-06-11 22:40:51 +0800

    gitsubmit

---
+++ src/main/java/com/pingan/haibin/HaibinMavenApplication.java
@@ -0,0 +1,13 @@
+package com.pingan.haibin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaibinMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaibinMavenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pingan/haibin/action/TestAction.java
@@ -0,0 +1,27 @@
+package com.pingan.haibin.action;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class TestAction {
+	
+
+	  
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "hello zhangsan V0.2";
+    }
+    
+    
+    @RequestMapping("/404.do")
+    public Object error_404() {
+        return "你要找的页面，被lison偷吃了！";
+    }
+    
+	
+    
+}
