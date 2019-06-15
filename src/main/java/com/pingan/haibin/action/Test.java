ecebbd61135474271c375e05ccc20d8e47728ab9
mrdonghaibin <51708985+mrdonghaibin@users.noreply.github.com>
2019-06-15 12:11:39 +0800

    Update Test.java
    githb

--- src/main/java/com/pingan/haibin/action/Test.java
+++ src/main/java/com/pingan/haibin/action/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("1166MastertttV201920 ty jdd 2");
+		System.out.println("1166MastertttV201920 ty jdd 20gitHub");
 
 	}
 
