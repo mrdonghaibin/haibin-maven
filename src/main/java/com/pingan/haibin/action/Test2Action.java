bcf8efa104c9c06b72999fa535d0ca5fc179fc66
mrdonghaibin <donghaibin@yeah.net>
2019-06-16 22:42:25 +0800

    t

---
+++ src/main/java/com/pingan/haibin/action/Test2Action.java
@@ -0,0 +1,38 @@
+package com.pingan.haibin.action;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+
+@RestController
+public class Test2Action {
+	
+/*	  @Resource
+	   private Jedis jedis;
+	   
+	    @RequestMapping("/jedis")
+	    public String jedis() {
+	    	String name = jedis.get("name");
+	        return name;
+	    }*/
+	  
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "hello Jenkins";
+    }
+    
+    
+    @RequestMapping("/404.do")
+    public Object error_404() {
+        return "你要找的页面，被lison偷吃了！";
+    }
+    
+	
+    
+}
