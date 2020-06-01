a19a06fbb101911a48b5ba483b6c072816d91592
mrdonghaibin <donghaibin@yeah.net>
2020-06-01 21:26:14 +0800

    donghaibin commited

--- src/main/java/com/pingan/haibin/action/Test.java
+++ src/main/java/com/pingan/haibin/action/Test.java
@@ -1,14 +1,33 @@
 package com.pingan.haibin.action;
 
+import java.util.Arrays;
+import java.util.List;
+
+
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 
-		System.out.println("1166MastertttV201920 ty jdd 28888");
+		String data = "59399.5";
+		
+		 //判断data是否为数值型
+        boolean isNum = data.toString().matches("^(-?\\d+)(\\.\\d+)?$");
+        //判断data是否为整数（小数部分是否为0）
+        boolean isInteger=data.toString().matches("^[-\\+]?[\\d]*$");
 
 
+        System.out.println("1166MastertttV201920 ty jdd isNum:"+isNum+",isInteger:"+isInteger);
+        
+        
+        String [] str = {"beijing","tianjin","haibin"};
+        List list = Arrays.asList(str);
+        System.out.println("beijing:"+list.contains("beijing2"));
+        if(list.contains("beijing")) {
+        	
+        }
 	}
 
 }
