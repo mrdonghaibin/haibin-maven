68844eef0d0a224640af7f83c884707b97109b3b
mrdonghaibin <donghaibin@yeah.net>
2020-06-08 21:28:36 +0800

    idea test5Y

--- src/main/java/com/pingan/haibin/action/Test2Action.java
+++ src/main/java/com/pingan/haibin/action/Test2Action.java
@@ -24,7 +24,7 @@ public class Test2Action {
 	  
     @RequestMapping("/hello")
     public String sayHello() {
-        return "hello Jenkins tttyU3383jfVyyyersion3";
+        return "hello Jenkins tttyU3383jfVersionH";
     }
 
     
